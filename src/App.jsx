cc9644686943ccde214029256054d7b8ba25bed9
AlejoTPK <alejandrocastrilloncar@gmail.com>
2024-05-28 22:57:28 -0500

    Se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import PlayerTable from './components/PlayerTable.jsx';
+import NavBarHome from './components/NavBarHome.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SocialMedial from './components/SocialMedial.jsx';
+
+
+
+function App() {
+
+  return (
+    <>
+      <NavBarHome />
+      <PlayerTable />
+      <SocialMedial />
+    </>
+  )
+}
+
+export default App
